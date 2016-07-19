52f84299f9b17d36275e837fd13263cc5141234a
TonyQ <tonylovejava@gmal.com>
2016-07-19 21:01:52 +0800

    finetune

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -19,23 +19,39 @@
     Marker Name: <input type="text" name="title" value="<?=h($fiddle_title)?>" />
       <button class="js-saveOrUpdate btn btn-default">Save</button>
       <!-- <button class="js-fork btn btn-default">Fork</button> -->
+
   </p>
+
   <div id="mapid" style="width: 100%; height: 600px"></div>
-
   
-  <div id='control' data-points="" style="position:fixed;padding:10px;left:30px;top:140px;background:white;border-radius:5px;opacity:0.9;min-width:340px;">
-    <button data-type="0" class="js-type btn btn-primary">Point (點) </button>
-    <button data-type="1" class="js-type btn btn-default">Line (線) </button>
-    <button data-type="2" class="js-type btn btn-default ">Area (區) </button>
-    <ul class="points" style="margin-top:10px;text-align:left;">
-     
-    </ul>
-    <button class="js-close btn btn">Close Line/Area</button>
+  
+  <div id='control' data-points="" style="z-index: 1000;position:fixed;padding:10px;left:30px;top:140px;background:#EEEEEE;border-radius:5px;opacity:0.9;min-width:340px;">
     
-    <p id="status"></p>
+    <div class="header">
+      <a class="js-close-control" style='float:right;font-size:20px;text-decoration: none;' href="">－</a>
+      <button data-type="0" class="js-type btn btn-default">Point (點) </button>
+      <button data-type="1" class="js-type btn btn-default">Line (線) </button>
+      <button data-type="2" class="js-type btn btn-default ">Area (區) </button>
+      
+      <div style="clear:both"></div>
+    </div>
 
-    <hr />
-    Power by <a href="https://github.com/tony1223/" target="_blank">TonyQ</a>
+    <div class="body" style="margin-top:10px;">
+      <p>
+        <input name="search" type="text" /> <button class="js-search-btn btn btn-default" >搜尋地址</button>
+        <div id="search-result"></div>
+      </p>
+      <ul class="points" style="margin-top:10px;text-align:left;">
+       
+      </ul>
+      <a class="js-close btn btn">Close Line/Area</a>
+      <br />
+      <a class="js-clear btn btn">Clear all points</a>
+      
+      <p id="status"></p>
+      <hr />
+      Power by <a href="https://github.com/tony1223/" target="_blank">TonyQ</a>
+    </div>
   </div>
 
 
@@ -51,9 +67,9 @@
       insert_index:null,
       points:window.points,
       currentMarkers:[],
+      tips:[],
+      tipMakers:[],
       changeType:function(type){
-
-
         if(type == 0){
           if(this.points.length > 1){
 
@@ -110,12 +126,37 @@
         this.points.splice(ind, 1);
         this.render();
       },
+      clearPoints:function(){
+        this.points = [];
+        this.render();
+      },
+      setCenter:function(latlng){
+        latlng && this.map.panTo(new L.LatLng(latlng[0],latlng[1]));
+      },
+      setTips:function(tips){
+        this.tips = tips;
+        this.render();
+      },
       render:function(){
         var that= this;
         this.currentMarkers.forEach(function(m){
           that.map.removeLayer(m);
         });
-        this.currentMarkers = [];
+        this.currentMarkers = []; 
+
+        this.tipMakers.forEach(function(m){
+          that.map.removeLayer(m);
+        });
+        this.tips.forEach(function(p){
+          var marker = L.circle(p.latlng,30,{
+              color: 'red',
+              fillColor: '#f03',
+              fillOpacity: 0.5
+          }).addTo(that.map);
+
+          marker.bindPopup(p.address);
+          that.currentMarkers.push(marker);
+        });
 
         if(this.type == 0 || this.points.length == 1){
           this.points.forEach(function(p){
@@ -183,8 +224,14 @@
       map.changeType(type);
     });
 
-    $(".js-close").on("click",function(){
+    $(".js-close").on("click",function(e){
       map.closeLineOrArea();
+      e.preventDefault();
+    });
+    
+    $(".js-clear").on("click",function(e){
+      map.clearPoints();
+      e.preventDefault();
     });
 
     $(".points").on("click",".js-item-delete",function(){
@@ -218,6 +265,65 @@
         }
         self.location.href='/marker/'+ res.data.key;
       })
+    });
+
+
+    var searchGEOCoding = function(){
+      var search = $("[name=search]").val();
+
+      $.get("https://maps.googleapis.com/maps/api/geocode/json",{
+        address:$("[name=search]").val(),
+        key:"AIzaSyBSIFJslwcgjr4ttFgt0TX3KSG6sqLkzY8"
+      },function(res){
+        if(res.status != "OK"){
+          alert("search fail");
+          return true;
+        }
+
+        var items = res.results.slice(0,3).map(function(r){
+          return {
+            latlng:[r.geometry.location.lat,r.geometry.location.lng],
+            address:r.formatted_address
+          };
+        });
+
+        map.setTips(items);
+
+        var out = items.map(function(i){
+          return "<li><a  class='js-search-item' data-latlng='"+i.latlng[0]+","+i.latlng[1]+"' href='#'>"+i.address+"</a></li>"
+        });
+        $("#search-result").html("<ul>"+out.join("")+"</ul>");
+
+        var jqs = $(".js-search-item");
+        if(jqs.length){
+          jqs[0].click();
+        }
+      });
+    }
+    $("[name=search]").on("keyup",function(e){
+      if(e.keyCode == 13){ 
+        searchGEOCoding();
+      }
+    });
+
+    $(".js-search-btn").click(function(){
+      searchGEOCoding();
+    });
+
+    $("#search-result").on("click",".js-search-item",function(e){
+      map.setCenter($(this).data("latlng").split(","));
+      e.preventDefault();
+    });
+
+    $(".js-close-control").on("click",function(){
+      var close = $(".js-close-control").html() =="－";
+      if(close){
+        $("#control .body").hide();        
+        $(".js-close-control").html("＋");
+      }else{
+        $("#control .body").show();        
+        $(".js-close-control").html("－");
+      }
     });
   </script>
   <script>
