14f1827043fadaa065d964515a50d483e65695ae
root <root@ip-172-31-21-165.ap-northeast-1.compute.internal>
2019-05-29 17:41:55 +0800

    ifinetune

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -402,7 +402,10 @@
       $(".js-close-control").click();
     }
 
-    var mymap = L.map('mapid').setView(center,  window.zoom || 16);
+    var mymap = L.map('mapid',{
+        minZoom: 0,
+        maxZoom: 22
+    }).setView(center,  window.zoom || 16);
 
     
     map.bind(mymap);
@@ -412,7 +415,7 @@
     map.render();
 
     var osm = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      maxZoom: window.m_zoom || 21,
+      maxZoom: window.m_zoom || 25,
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
